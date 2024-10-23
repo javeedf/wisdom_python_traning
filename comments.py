5f4b226dde4aab055c99cc79e8cb1d49e67afaad
Mohamed Javeed <javeedf@gmail.com>
2024-10-23 19:55:07 +0530

    Create comments.py

---
+++ comments.py
@@ -0,0 +1,47 @@
+# This is a single-line comment
+print("Hello, World!")  # This is an inline comment
+
+# Multiple single-line comments
+# can be used to create a block of comments
+# explaining the following code
+print('Hello again!')
+
+# This is a single-line comment
+print("Hello, World!")  # This is an inline comment
+
+# Multiple single-line comments
+# can be used to create a block of comments
+# explaining the following code
+print("Hello again!")
+
+"""
+This is a multi-line comment
+using triple-quoted strings.
+It can span multiple lines.
+"""
+print("Hello, once more!")
+
+def greet(name):
+    """
+    This function greets the person
+    whose name is passed as a parameter.
+    """
+    print(f"Hello, {name}!")
+
+greet("Alice")
+
+
+'''
+This is a multi-line comment
+using triple-quoted single quotes.
+It can span multiple lines.
+'''
+print('Hello, once more!')
+
+def greet(name):
+    '''
+    This function greets the person
+    whose name is passed as a parameter.
+'''
+    print(f'Hello, {name}!')
+
